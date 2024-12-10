99bb4e07426bb0f118e2ea610376141674d32e6f
Nikita Karmakar <karmakarnikita412@gmail.com>
2024-12-10 13:19:22 +0530

    Update hof.js

--- hof.js
+++ hof.js
@@ -15,3 +15,27 @@ function f(x, fn) {
 f(10, function exec(){
     console.log("I am an expression passed to a HOF")
 })
+
+let arr = [1,10,9,100,100,11,12,13,14,2,3]; // unsorted array
+
+arr.short(); // it sorts the given array // [exception] -> this might arrange elements in inc order
+// default sorting is done in lexicographical order
+// default implementation of arr.sort() is going to sort my array in lexicographical order
+/**
+ * 0 -> A
+ * 1 -> B
+ * 2 -> C
+ * 3 -> D
+ * 4 -> E
+ * 5 -> F
+ * 6 -> G
+ * 7 -> H
+ * 8 -> I
+ * 9 -> J
+ * 10 -> K
+ * ...........
+ * [B, BA, J, BAA, BAAA, BB, BC, BD, BE, B, C] //if we arrange it according to dictionary
+ * [B, BA, BAA, BAAA, BB, BC, BD .............] //if we arrange it according to dictionary
+ * 
+ * */ 
+console.log(arr);
